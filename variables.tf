fcbf0196334f99fb7cd289c2769011cffba1c574
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-07 10:34:45 +0530

    terraform-vpc-module

--- variables.tf
+++ variables.tf
@@ -12,9 +12,9 @@ variable "vpc_cidr" {
 
 variable "common_tags" {
   default = {
-    Project = "expense"
+    Project     = "expense"
     Environment = "dev"
-    Terraform = "true"
+    Terraform   = "true"
   }
 }
 
@@ -23,3 +23,16 @@ variable "vpc_tags" {
     Purpose = "assignment"
   }
 }
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "database_subnet_cidrs" {
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,12 @@
 module "vpc" {
-  source = "../terraform-vpc-module"
-  project_name = var.project_name
-  environment = var.environment
-  vpc_cidr = var.vpc_cidr
-  common_tags = var.common_tags
-  vpc_tags = var.vpc_tags
+  source                = "../terraform-vpc-module"
+  project_name          = var.project_name
+  environment           = var.environment
+  vpc_cidr              = var.vpc_cidr
+  common_tags           = var.common_tags
+  vpc_tags              = var.vpc_tags
+  public_subent_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
+  database_subnet_cidrs = var.database_subnet_cidrs
+
 }
