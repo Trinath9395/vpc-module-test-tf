9ca0a7623192ebd59ff6c70d766050c1c78dce31
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-07 11:07:30 +0530

    terraform-vpc-module

--- vpc.tf
+++ vpc.tf
@@ -5,8 +5,7 @@ module "vpc" {
   vpc_cidr              = var.vpc_cidr
   common_tags           = var.common_tags
   vpc_tags              = var.vpc_tags
-  public_subent_cidrs   = var.public_subnet_cidrs
+  public_subnet_cidrs   = var.public_subnet_cidrs
   private_subnet_cidrs  = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
-
 }
